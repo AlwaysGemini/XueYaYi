98e07ce4984c7b103d3332c00cde16e6fed017fa
AlwaysGemini <185547890@qq.com>
2018-10-17 20:05:44 +0800

    血压仪app初步框架

---
+++ app/src/main/java/com/example/administrator/xueyayi/Bespeak_Activity.java
@@ -0,0 +1,71 @@
+package com.example.administrator.xueyayi;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class Bespeak_Activity extends AppCompatActivity {
+
+    Button select_date = null;
+    int mYear;
+    int mMonth;
+    int mDay;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bespeak_);
+        Calendar ca = Calendar.getInstance();
+        mYear = ca.get(Calendar.YEAR);
+        mMonth = ca.get(Calendar.MONTH);
+        mDay = ca.get(Calendar.DAY_OF_MONTH);
+
+        select_date = findViewById(R.id.select_date);
+        select_date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO 调用时间选择器
+                new DatePickerDialog(Bespeak_Activity.this, onDateSetListener, mYear, mMonth, mDay).show();
+            }
+        });
+    }
+
+    private DatePickerDialog.OnDateSetListener onDateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            mYear = year;
+            mMonth = monthOfYear;
+            mDay = dayOfMonth;
+            String days;
+            if (mMonth + 1 < 10) {
+                if (mDay < 10) {
+                    days = new StringBuffer().append(mYear).append("年").append("0").
+                            append(mMonth + 1).append("月").append("0").append(mDay).append("日").toString();
+                } else {
+                    days = new StringBuffer().append(mYear).append("年").append("0").
+                            append(mMonth + 1).append("月").append(mDay).append("日").toString();
+                }
+
+            } else {
+                if (mDay < 10) {
+                    days = new StringBuffer().append(mYear).append("年").
+                            append(mMonth + 1).append("月").append("0").append(mDay).append("日").toString();
+                } else {
+                    days = new StringBuffer().append(mYear).append("年").
+                            append(mMonth + 1).append("月").append(mDay).append("日").toString();
+                }
+
+            }
+            TextView date=findViewById(R.id.date);
+            date.setText(days);
+        }
+    };
+
+}
